a66ec55c9b92a56bd6781e3f0e1d4b6eec290267
Adrian Olczak <adrianolczakdev@gmail.com>
2023-02-05 00:32:19 +0100

    account: extend profile model

--- aoblog/account/models.py
+++ aoblog/account/models.py
@@ -16,6 +16,12 @@ def user_directory_path(instance, filename):
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to=user_directory_path, blank=True)
+    bio = models.CharField(max_length=150, blank=True)
+    website_link = models.URLField(max_length=65, blank=True)
+    github_link = models.URLField(max_length=65, blank=True)
+    twitter_link = models.URLField(max_length=65, blank=True)
+    instagram_link = models.URLField(max_length=65, blank=True)
+    facebook_link = models.URLField(max_length=65, blank=True)
 
     def __str__(self):
         return f'Profile of {self.user.username}'
