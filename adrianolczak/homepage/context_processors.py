2c7c2f4951f9e18b09852ab8e3b7aca16279d2e1
Adrian Olczak <adrianolczakdev@gmail.com>
2023-03-08 12:12:12 +0100

    add social media context processor and update templates

---
+++ adrianolczak/homepage/context_processors.py
@@ -0,0 +1,9 @@
+def social_media(request):
+    return {
+        'facebook': '#',
+        'instagram': '#',
+        'twitter': '#',
+        'linkedin': '#',
+        'google': '#',
+        'github': 'https://github.com/adrolc',
+    }
