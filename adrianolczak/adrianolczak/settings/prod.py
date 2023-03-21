259f3d7c67cc885ed2a9ee6bb03c53290080b2b7
Adrian Olczak <adrianolczakdev@gmail.com>
2023-03-21 20:14:20 +0100

    email settings for production

--- adrianolczak/adrianolczak/settings/prod.py
+++ adrianolczak/adrianolczak/settings/prod.py
@@ -25,5 +25,14 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+DEFAULT_FROM_EMAIL = 'blog@adrianolczak.pl'
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+
+
 SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
