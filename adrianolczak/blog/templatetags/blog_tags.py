d18beaf05f6ef569c7410fa161d654abc00e9f83
Adrian Olczak <adrianolczakdev@gmail.com>
2023-10-03 13:12:18 +0200

    refactoring blog template tags

--- adrianolczak/blog/templatetags/blog_tags.py
+++ adrianolczak/blog/templatetags/blog_tags.py
@@ -1,36 +1,34 @@
-from django import template
-from django.db.models import Count
-from django.utils.safestring import mark_safe
 import markdown
-from ..models import Post
+from django import template
+from django.utils.safestring import mark_safe
 from taggit.models import Tag
 
+from ..models import Post
 
 register = template.Library()
 
 
-@register.simple_tag
-def total_posts():
-    return Post.published.count()
-
-@register.inclusion_tag('blog/inclusion_tag_templates/latest_posts.html')
+@register.inclusion_tag("blog/includes/tt_latest_posts.html")
 def show_latest_posts(count=3):
-    latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts': latest_posts}
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
 
-@register.simple_tag
-def get_most_commented_posts(count=3):
-    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
-@register.inclusion_tag('blog/inclusion_tag_templates/tags.html')
-def show_tags():
+@register.inclusion_tag("blog/includes/tt_all_tags.html")
+def show_all_tags():
     tags = Tag.objects.all()
-    return {'tags': tags}
+    return {"tags": tags}
 
-@register.filter(name='markdown')
+
+@register.filter(name="markdown")
 def markdown_format(text):
-    return mark_safe(markdown.markdown(text, extensions=[
-        'markdown.extensions.fenced_code',
-        'markdown.extensions.extra',
-        'markdown.extensions.attr_list'
-    ]))
+    return mark_safe(
+        markdown.markdown(
+            text,
+            extensions=[
+                "markdown.extensions.fenced_code",
+                "markdown.extensions.extra",
+                "markdown.extensions.attr_list",
+            ],
+        )
+    )
