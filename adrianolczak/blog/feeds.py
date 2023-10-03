b70e13284593674958faeee61cb9962751044ef6
Adrian Olczak <adrianolczakdev@gmail.com>
2023-10-03 13:06:50 +0200

    rss - minor refactoring

--- adrianolczak/blog/feeds.py
+++ adrianolczak/blog/feeds.py
@@ -2,13 +2,14 @@ import markdown
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords_html
 from django.urls import reverse_lazy
+
 from .models import Post
 
 
 class LatestPostsFeed(Feed):
-    title = 'Tech Blog - adrianolczak.pl'
-    link = reverse_lazy('blog:home')
-    description = 'New posts of Tech Blog - adrianolczak.pl'
+    title = "Tech Blog - adrianolczak.pl"
+    link = reverse_lazy("blog:home")
+    description = "Latest Posts"
 
     def items(self):
         return Post.published.all()[:4]
@@ -17,7 +18,12 @@ class LatestPostsFeed(Feed):
         return item.title
 
     def item_description(self, item):
-        return truncatewords_html(markdown.markdown(item.body, extensions=['markdown.extensions.fenced_code']), 35)
+        return truncatewords_html(
+            markdown.markdown(
+                item.body, extensions=["markdown.extensions.fenced_code"]
+            ),
+            35,
+        )
 
     def item_pubdate(self, item):
         return item.publish
--- adrianolczak/blog/urls.py
+++ adrianolczak/blog/urls.py
@@ -7,7 +7,7 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="home"),
-    path("tag/<slug:tag_slug>/", views.PostListView.as_view(), name="post_list_by_tag"),
-    path("<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("tag/<slug:slug>/", views.PostListView.as_view(), name="post_list_by_tag"),
     path("feed/", LatestPostsFeed(), name="post_feed"),
+    path("<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
 ]
